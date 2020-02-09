6c82f88e6c16e87124fe4c6bd8d10f7755e830fd
Anirudh <anirudhbalakka@gmail.com>
2020-02-09 18:59:13 +0530

    Added type checking | Custom Creator | Fixed dependency versions

--- APM-Demo0/src/app/app.module.ts
+++ APM-Demo0/src/app/app.module.ts
@@ -17,13 +17,23 @@ import { PageNotFoundComponent } from './home/page-not-found.component';
 /* Feature Modules */
 import { UserModule } from './user/user.module';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(ProductData),
     UserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: 'APM Demo App Devtools',
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   declarations: [
     AppComponent,
---
+++ APM-Demo0/src/app/state/app.state.ts
@@ -0,0 +1,5 @@
+import { ProductState } from '../products/store/product.reducer';
+
+export interface State {
+    user: any;
+}
